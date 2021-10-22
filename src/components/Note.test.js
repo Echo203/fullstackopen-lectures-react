39c932b641dd02b3e92004de093901206476addd
Echo203 <75540650+Echo203@users.noreply.github.com>
2021-10-22 17:28:26 +0200

    Test for click handling

--- src/components/Note.test.js
+++ src/components/Note.test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
-import { prettyDOM } from '@testing-library/dom'
+import { render, fireEvent } from '@testing-library/react'
 import Note from './Note'
 
 test('Renders Content', () => {
@@ -15,4 +14,22 @@ test('Renders Content', () => {
   )
 
   expect(component.container).toHaveTextContent('Component testing with react testing library')
+})
+
+test('Clicking button calls event handler once', () => {
+  const note = {
+    content: 'Component testing with react testing library',
+    important: true
+  }
+
+  const mockHandler = jest.fn()
+
+  const component = render(
+    <Note note={note} toggleImportance={mockHandler}/>
+  )
+
+  const button = component.getByText('make not important')
+  fireEvent.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
