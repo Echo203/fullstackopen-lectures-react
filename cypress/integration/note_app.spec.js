af58d613103c29f3a8a3dda03cb780a9c56b19ae
Echo203 <75540650+Echo203@users.noreply.github.com>
2021-10-25 19:00:08 +0200

    test: making note important

--- cypress/integration/note_app.spec.js
+++ cypress/integration/note_app.spec.js
@@ -1,5 +1,11 @@
 describe('Note app', function() {
   beforeEach(function() {
+    cy.request('POST', 'http://localhost:3000/api/testing/reset')
+    const user = {
+      username: 'test',
+      password: 'test'
+    }
+    cy.request('POST', 'http://localhost:3000/api/users', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -19,5 +25,38 @@ describe('Note app', function() {
     cy.get('#login-button').click()
 
     cy.contains('Logged as test')
+  })
+
+  describe('When logged in', function() {
+    beforeEach(function() {
+      cy.contains('Login').click()
+      cy.get('#username').type('test')
+      cy.get('#password').type('test')
+      cy.get('#login-button').click()
+    })
+
+    it('new note can be added', function() {
+      cy.contains('Add note').click()
+      cy.get('input').type('New note added by cypress')
+      cy.contains('save').click()
+      cy.contains('New note added by cypress')
+    })
+
+    describe('and a note exist', function() {
+      beforeEach(function () {
+        cy.contains('Add note').click()
+        cy.get('input').type('another cypress note')
+        cy.contains('save').click()
+      })
+
+      it('it can be made important', function() {
+        cy.contains('another cypress note')
+          .contains('make important')
+          .click()
+
+        cy.contains('another cypress note')
+          .contains('make not important')
+      })
+    })
   })
 })
